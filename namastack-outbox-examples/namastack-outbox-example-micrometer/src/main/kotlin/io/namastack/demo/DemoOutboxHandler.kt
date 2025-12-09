ce2331e58b0cd4f0680f159ac3d33d29b91d66ce
Aleksander Zamojski <zamojski96@gmail.com>
2025-12-09 10:51:26 +0100

    Add micrometer example

---
+++ namastack-outbox-examples/namastack-outbox-example-micrometer/src/main/kotlin/io/namastack/demo/DemoOutboxHandler.kt
@@ -0,0 +1,30 @@
+package io.namastack.demo
+
+import io.micrometer.observation.annotation.Observed
+import io.namastack.demo.customer.CustomerRegisteredEvent
+import io.namastack.outbox.annotation.OutboxHandler
+import io.namastack.outbox.handler.OutboxRecordMetadata
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+@Component
+class DemoOutboxHandler {
+    private val logger = LoggerFactory.getLogger(DemoOutboxHandler::class.java)
+
+    @Observed
+    @OutboxHandler
+    fun handle(
+        payload: Any,
+        metadata: OutboxRecordMetadata,
+    ) {
+        logger.info("[Handler] Publish {}: {}", payload::class.simpleName, metadata.key)
+        ExternalBroker.publish(payload, metadata.key)
+    }
+
+    @Observed
+    @OutboxHandler
+    fun handle(payload: CustomerRegisteredEvent) {
+        logger.info("[Handler] Send email to: {}", payload.email)
+        ExternalMailService.send(payload.email)
+    }
+}
