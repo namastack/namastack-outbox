1f509a1ed6e44614f319a47ccef041cdd6bbc359
Aleksander Zamojski <zamojski96@gmail.com>
2025-12-19 10:28:44 +0100

    fix: downgrade Java version to 21 to match minimum Java requirement (#123)

--- namastack-outbox-examples/namastack-outbox-example-java/src/main/java/io/namastack/demo/DemoApplication.java
+++ namastack-outbox-examples/namastack-outbox-example-java/src/main/java/io/namastack/demo/DemoApplication.java
@@ -23,7 +23,7 @@ public class DemoApplication implements CommandLineRunner {
     this.service = service;
   }
 
-  static void main(String[] args) {
+  public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
   }
 
