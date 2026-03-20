94017d0a7de42cdde291b97629ab2b942454dfc0
Roland Beisel <info@rolandbeisel.de>
2026-03-20 22:58:32 +0100

    update docs for release 1.2.0

--- namastack-outbox-docs/docusaurus.config.ts
+++ namastack-outbox-docs/docusaurus.config.ts
@@ -1,7 +1,6 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
-import remarkVariables from './src/plugins/remark-variables';
 
 const config: Config = {
   title: 'Namastack Outbox',
